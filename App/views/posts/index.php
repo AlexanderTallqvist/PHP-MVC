f3be554ee900b34c3c69e0d860d02a1be5603c5e
Alexander Tallqvsit <alexander_tallqvist@hotmail.com>
2018-10-25 21:56:36 +0300

    More styles for various components of the site.

--- App/views/posts/index.php
+++ App/views/posts/index.php
@@ -1,15 +1,15 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
   <div class="posts--main">
-    <h1 class="posts--main__title">All the Posts</h1>
-    <a href="<?php echo URLROOT; ?>/posts/add">Add post</a>
-    <div class="posts--main__list">
+    <h1 class="posts--main--title">Recent Posts</h1>
+    <a class="posts--main--add" href="<?php echo URLROOT; ?>/posts/add">Add post</a>
+    <div class="posts--main--list">
       <?php foreach($data['posts'] as $post) : ?>
-        <div class="posts--main__list__item">
-          <h2 class="posts--main__list__item__title"><?php echo $post->title; ?></h2>
-          <span class="posts--main__list__item__author">Author: <?php echo $post->name; ?></span>
-          <span class="posts--main__list__item__created">Created: <?php echo $post->postCreated; ?></span>
-          <div class="posts--main__list__item__content"><?php echo $post->body; ?></div>
-          <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="posts--main__list__item__more">Read more</a>
+        <div class="posts--main--list__item">
+          <h2 class="posts--main--list__item__title"><?php echo $post->title; ?></h2>
+          <span class="posts--main--list__item__author"><span>Author:</span> <?php echo $post->name; ?></span>
+          <span class="posts--main--list__item__created"><span>Created:</span> <?php echo $post->postCreated; ?></span>
+          <div class="posts--main--list__item__content"><?php echo $post->body; ?></div>
+          <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="posts--main--list__item__more">Read more</a>
         </div>
       <?php endforeach; ?>
     </div>
